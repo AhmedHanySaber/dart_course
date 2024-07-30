7f34c9d0fcf107df2fa404713b4e239fe247c2d6
Ahmed Hany Saber Abo Zed Ahmed <130825620+AhmedHanySaber@users.noreply.github.com>
2024-07-31 02:09:27 +0300

    start

---
+++ intro_to_dart.dart
@@ -0,0 +1,107 @@
+// void main() { 
+//    print("Hello World!"); 
+// }
+
+// void main()
+// {
+//     var name = "John";
+//     print(name);
+// }
+
+// void main(){
+//   var firstName = "John";
+//   var lastName = "Doe";
+//   print("Full name is $firstName $lastName");
+// }
+
+// void main() {
+// int num1 = 10; //declaring number1
+// int num2 = 3; //declaring number2
+  
+// // Calculation
+// int sum = num1 + num2;
+// int diff = num1 - num2;
+// int mul = num1 * num2;
+// double div = num1 / num2; // It is double because it outputs number with decimal.
+  
+// // displaying the output
+// print("The sum is $sum");
+// print("The diff is $diff");
+// print("The mul is $mul");
+// print("The div is $div");
+// }
+
+// void main() {
+// // Declaring Variables  
+// int num1 = 100; // without decimal point.
+// double num2 = 130.2; // with decimal point.
+// num num3 = 50;
+// num  num4 = 50.4;  
+
+// // For Sum   
+// num sum = num1 + num2 + num3 + num4;
+   
+// // Printing Info   
+// print("Num 1 is $num1");
+// print("Num 2 is $num2");  
+// print("Num 3 is $num3");  
+// print("Num 4 is $num4");  
+// print("Sum is $sum");  
+   
+// }
+
+// void main() {
+// // Declaring Values     
+// String schoolName = "Diamond School";
+// String address = "New York 2140";   
+
+// // Printing Values
+// print("School name is $schoolName and address is $address");   
+// }
+
+
+// void main() {
+// // Multi Line Using Single Quotes   
+// String multiLineText = '''
+// This is Multi Line Text
+// with 3 single quote
+// I am also writing here.
+// ''';
+   
+// // Multi Line Using Double Quotes   
+// String otherMultiLineText = """
+// This is Multi Line Text
+// with 3 double quote
+// I am also writing here.
+// """;
+   
+// // Printing Information   
+// print("Multiline text is $multiLineText");
+// print("Other multiline text is $otherMultiLineText");
+// }
+
+// void main() {
+// List<String> names = ["Raj", "John", "Max"];
+// print("Value of names is $names");
+// print("Value of names[0] is ${names[0]}"); // index 0
+// print("Value of names[1] is ${names[1]}"); // index 1
+// print("Value of names[2] is ${names[2]}"); // index 2
+
+//   // Finding Length of List 
+// int length = names.length;  
+// print("The Length of names is $length");
+// }
+// void main() {
+// Set<String> weekday = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+// print(weekday);
+// }
+
+// void main() {
+// Map<String, String> myDetails = {
+//    'name': 'John Doe',
+//    'address': 'USA',
+//    'fathername': 'Soe Doe'
+// };
+// // displaying the output
+// print(myDetails['name']);
+// }
